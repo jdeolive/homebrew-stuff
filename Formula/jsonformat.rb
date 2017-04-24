aa5b264695a15a639d805a40609169c97ff4f53b
jdeolive <jdeolive@gmail.com>
2017-04-23 20:26:04 -0600

    Changing formulas to use sha256

--- Formula/jsonformat.rb
+++ Formula/jsonformat.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Jsonformat < Formula
   homepage 'https://github.com/jdeolive/jsonformat'
   url 'https://github.com/jdeolive/jsonformat/archive/v1.2.tar.gz'
-  sha1 '379565929eda8555888d366bca2bc6a3ee59fd7e'
+  sha256 'e3cf80d1a36d456ed8ec0e9c0dd5a758f92f461347c3bcff63f9928bef74ad89'
 
   def install
     bin.install "jsonformat.py" => "jsonformat"
--- Formula/shorty.rb
+++ Formula/shorty.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Shorty < Formula
   homepage 'https://github.com/jdeolive/shorty'
   url 'https://github.com/jdeolive/shorty/archive/v1.0.tar.gz'
-  sha1 '24fa16623ba4b7ca41025d933baa6e5cee11a469'
+  sha256 'a43397080abdd6c165bf4d59986b12d7546e436ea2aaa9dd9496cc38218c4eb2'
 
   def install
     bin.install "shorty" => "shorty"
